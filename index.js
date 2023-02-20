83d0ebdb5487cee3e28a80cf915406bb4812def4
John Doe <sakthivel8725@gmail.com>
2023-02-20 11:42:23 +0530

    feat: Backend Server Is Done with nodejs.

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+//enable body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/openai", require("./routes/openaiRoutes"));
+
 app.listen(port, () => console.log(`Server Started on ${port}`));
